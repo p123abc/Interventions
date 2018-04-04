273fea5bb42488c98d432a538cd5198d8683708d
Patrick Papineau <patpap002gmail.com>
2018-04-04 15:15:22 -0400

    update test sansEspace

--- src/app/shared/caracteres-validator.spec.ts
+++ src/app/shared/caracteres-validator.spec.ts
@@ -1,9 +1,25 @@
 import { VerifierNombresValidator } from "./caracteres-validator";
+import { AbstractControl } from "@angular/forms";
 
-describe('sans espace', ()=> {
+describe('sansEspaces Validator', () => {
     it('plage pour la valeur valide limite 1', () => {
-        let validator = VerifierNombresValidator.plage();
-        let result= validator(null);
-        expect(result['plage']).toBe(true);
-    })
+        let control = {value: ''};
+        let validator = VerifierNombresValidator.sansEspaces();
+        let result= validator(control as AbstractControl);
+        expect(result['sansEspaces']).toBe(false);
+    });
+    
+    it('plage pour la valeur valide limite 1', () => {
+        let control = {value: '          '};
+        let validator = VerifierNombresValidator.sansEspaces();
+        let result= validator(control as AbstractControl);
+        expect(result['sansEspaces']).toBe(false);
+    });
+
+    it('plage pour la valeur valide limite 0', () => {
+        let control = {value: '   '};
+        let validator = VerifierNombresValidator.sansEspaces();
+        let result= validator(control as AbstractControl);
+        expect(result['sansEspaces']).toBe(false);
+    });
 })
--- src/app/shared/caracteres-validator.ts
+++ src/app/shared/caracteres-validator.ts
@@ -1,9 +1,13 @@
-import { ValidatorFn } from "@angular/forms";
+import { ValidatorFn, AbstractControl } from "@angular/forms";
 
 export class VerifierNombresValidator {
-    static plage(): ValidatorFn {
-        return (): { [key: string]: boolean } | null => {
-            return { 'plage': true};
+    static sansEspaces(): ValidatorFn {
+        return (c: AbstractControl): { [key: string]: boolean } | null => {
+            // found something other than a space or line break
+            if (/\S/.test(c.value) && c.value !== null){
+                return{'sansEspaces': true};
+            }
+            return { 'sansEspaces': false};
         };
     }
 }
