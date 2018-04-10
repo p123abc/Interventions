679ade0121e5d639a4ec467138477ca5ba11b872
Patrick Papineau <patpap002gmail.com>
2018-04-10 12:17:13 -0400

    tp8 fini

--- src/app/shared/caracteres-validator.spec.ts
+++ src/app/shared/caracteres-validator.spec.ts
@@ -9,17 +9,24 @@ describe('sansEspaces Validator', () => {
         expect(result['sansEspaces']).toBe(false);
     });
     
-    it('plage pour la valeur valide limite 1', () => {
+    it('plage pour la valeur invalide avec 10 espaces', () => {
         let control = {value: '          '};
         let validator = VerifierNombresValidator.sansEspaces();
         let result= validator(control as AbstractControl);
         expect(result['sansEspaces']).toBe(false);
     });
 
-    it('plage pour la valeur valide limite 0', () => {
-        let control = {value: '   '};
+    it('plage pour la valeur valide avec une phrase avec des mots est valide', () => {
+        let control = {value: 'Une phrase avec des mots est valide'};
         let validator = VerifierNombresValidator.sansEspaces();
         let result= validator(control as AbstractControl);
-        expect(result['sansEspaces']).toBe(false);
+        expect(result['sansEspaces']).toBe(true);
+    });
+
+    it('plage pour la valeur valide avec une phrase avec 3 espaces, des mots et ensuite 3 espaces est valide ', () => {
+        let control = {value: '      une phrase avec 3 espaces, des mots et ensuite 3 espaces est valide      '};
+        let validator = VerifierNombresValidator.sansEspaces();
+        let result= validator(control as AbstractControl);
+        expect(result['sansEspaces']).toBe(true);
     });
 })
