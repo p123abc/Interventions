94341b5d162208bb623b74d41a0c27a7e8d2ba51
Patrick Papineau <patpap002gmail.com>
2018-04-11 14:59:28 -0400

    tp 11 non fini

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { VerifierNombresValidator } from '../shared/caracteres-validator';
+import { TypeService } from './type.service';
+import { IType } from './typeprobleme';
 
 @Component({
   selector: 'inter-probleme',
@@ -10,13 +12,20 @@ import { VerifierNombresValidator } from '../shared/caracteres-validator';
 export class ProblemeComponent implements OnInit {
 
   problemeForm: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  typeProbleme: IType[];
+  errorMessage: string;
+  constructor(private fb: FormBuilder, private type: TypeService) { }
 
   ngOnInit() {
     this.problemeForm = this.fb.group({
       prenom: ['',[Validators.required, VerifierNombresValidator.longueurMinimum(3)]],
-      nom: ['',[Validators.required, VerifierNombresValidator.longueurMinimum(3)]]
+      nom: ['',[Validators.required, VerifierNombresValidator.longueurMinimum(3)]],
+      noType:['']
     });
+
+    this.type.obtenirType()
+    .subscribe(cat => this.typeProbleme = cat,
+              error => this.errorMessage = <any>error);
   }
 
 }
--- src/app/probleme/type.service.ts
+++ src/app/probleme/type.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { IType } from './typeprobleme';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class TypeService {
+  private baseUrl = 'api/type';
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
+  obtenirType(): Observable<IType[]>{
+    return this._http.get<IType[]>(this.baseUrl)
+    .do(data => console.log('obtenirType: ' + JSON.stringify(data)))
+    .catch(this.handleError);
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    console.error(err.error);
+    return Observable.throw(err.message);
+  }
 }
